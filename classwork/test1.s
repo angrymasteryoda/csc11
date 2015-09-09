6721f95b9b1f191651fa76c0b458a6cbe058372a
Michael Risher <rishermichael@CSC11.(none)>
2015-09-09 11:52:46 -0700

    added comments to test1

--- classwork/test1.s
+++ classwork/test1.s
@@ -6,7 +6,7 @@
 
 .global _start
 _start:
-    mov R0, #65
-    mov R7, #1
-    swi 0
+    mov R0, #65 /* put 65 in register 0 */
+    mov R7, #1 /* put number 1 in register 7 */
+    swi 0 /* software interrupt */
 
