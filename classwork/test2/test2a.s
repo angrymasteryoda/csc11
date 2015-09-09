c323f1668d56ecca8dc99f1260025942d3883ba7
Michael Risher <rishermichael@CSC11.(none)>
2015-09-09 12:01:08 -0700

    spliting a program

---
+++ classwork/test2/test2a.s
@@ -0,0 +1,11 @@
+/*
+  michael risher
+  09/09/15
+  split a program
+*/
+
+.global _start
+_start:
+    mov R0, #65 /* put 65 into register 0 */
+
+
---
+++ classwork/test2/test2b.s
@@ -0,0 +1,11 @@
+/*
+   michael risher
+   09/09/15
+   split a program part 2
+*/
+
+
+.global _part2
+_part2:
+    mov r7, #1
+    swi 0
