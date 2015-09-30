210b2966bee254ca14312d03e91b73a1d50f2a3f
Michael Risher <rishermichael@gmail.com>
2015-09-30 11:27:24 -0700

    comments added

--- csc17c/linkedList/nbproject/Makefile-variables.mk
+++ csc17c/linkedList/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=linkedlist
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/linkedlist
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/linkedlist
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=linkedlist.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/linkedlist.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/linkedlist.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=linkedlist
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/linkedlist
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/linkedlist
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=linkedlist.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/linkedlist.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/linkedlist.tar
 #
 # include compiler specific variables
 #
--- homework/w5/shift div/shift.s
+++ homework/w5/shift div/shift.s
@@ -6,26 +6,26 @@ shifting division
 .global _start
 
 _start:
-    mov r0, #0
-    mov r1, #45
-    mov r2, #6
-    mov r3, #1
-    mov r4, r2
-    mov r5, #0
+    mov r0, #0		@result
+    mov r1, #45		@number
+    mov r2, #6		@divisor
+    mov r3, #1		@scale
+    mov r4, r2		@divisor that we edit
+    mov r5, #0		@counter
 shift:
-    lsl r4, #1
-    lsl r3, #1
+    lsl r4, #1		@shift left on divisor
+    lsl r3, #1		@shift scale
     cmp r4, r1
-    blt shift
-    lsr r4, #1
+    blt shift		@r4 < r1
+    lsr r4, #1		@else right shift to fix the over shift
     lsr r3, #1
 subtract:
-    sub r1, r1, r4
-    add r5, r5, r3
-    lsr r4, #1
-    lsr r3, #1
+    sub r1, r1, r4	@do subtract
+    add r5, r5, r3	@add to counter
+    lsr r4, #1		@shift right 
+    lsr r3, #1		@shift right
     cmp r4, r1
-    blt subtract
-    mov r0, r5
+    blt subtract	@r4 < r1
+    mov r0, r5		@done get ready to print
     mov r7, #1
     swi 0
