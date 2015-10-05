994698c527be3fb9b618ebe13a6ff7eab1e3cb6b
Michael Risher <rishermichael@gmail.com>
2015-10-05 11:48:11 -0700

    classwork

--- classwork/week6/sumfor.s
+++ classwork/week6/sumfor.s
@@ -2,16 +2,16 @@
 sum of 1 to 10
 */
 
-global _start:
+.global _start
 
 _start:
     mov r0, #0
     mov r1, #1   @counter
-    mov r2, #0
+    mov r2, #10
 loop:
     add r0, r0, r1
     add r1, r1, #1
     cmp r1, #10
-    bge loop
+    ble loop
     mov r7, #1
     swi 0
