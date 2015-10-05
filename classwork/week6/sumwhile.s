376c444f945e7e4a0700fa9bec48d3c8e7614e84
Michael Risher <rishermichael@gmail.com>
2015-10-05 11:50:43 -0700

    classwork

--- classwork/week6/sumwhile.s
+++ classwork/week6/sumwhile.s
@@ -6,12 +6,13 @@ sum of 1 to 10
 
 _start:
     mov r0, #0
-    mov r1, #1   @counter
-    mov r2, #10
+    mov r1, #1
 loop:
+    cmpr r1, #10
+    bge end
     add r0, r0, r1
     add r1, r1, #1
-    cmp r1, #10
-    ble loop
+    bal loop
+end:
     mov r7, #1
     swi 0
