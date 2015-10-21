054295bbd4937f28ef868acc0feb111539c74443
Michael Risher <rishermichael@gmail.com>
2015-10-21 13:59:44 -0700

    class

--- homework/w8/cToF.s
+++ homework/w8/cToF.s
@@ -2,7 +2,20 @@
 .balign 4
 msg: .asciz "Display Degree Centigrade to Degree Fahrenheit\nFahrenheit  Centigrade\n"
 
+test: .asciz "%d"
 .text
 
 .global cToF:
+
+/*
+r0, inputBeg
+r1, inputEnd
+*/
+cToF:
+	mov r2, =0x1CCCCCD @bp -4 9/5
+	mov r1, r0
+	ldr r0, =test
+	
+	
+.global printf
 	
