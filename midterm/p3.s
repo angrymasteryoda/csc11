0c63480246d78137bf86263d0a9472c76faca619
Michael Risher <rishermichael@gmail.com>
2015-10-26 12:10:25 -0700

    class

--- midterm/p3.s
+++ midterm/p3.s
@@ -2,7 +2,9 @@
 .balign 4
 inputNum: .word 0
 .balign 4
-inputMsg: .asciz "%d"
+inputMsg: .asciz "Input a number for fibbonacii: "
+.balign 4
+scanPat: .asciz "%d"
 .balign 4
 outMsg: .asciz "answer: %d\n"
 
@@ -12,13 +14,32 @@ outMsg: .asciz "answer: %d\n"
 main:
 	push {lr}
 	ldr r0, =inputMsg
+	bl printf
+	
+	ldr r0, =scanPat
 	ldr r1, =inputNum
 	bl scanf
 	
 	ldr r0, = outMsg
 	ldr r1, inputNumAddr
 	ldr r1, [r1]
+	
+	cmp r1, #1 ;r1 <= 1
+	ble print
+	mov r2, #1 ;fibo
+	mov r3, #1 ;fibo previous
+	mov r4, #2 ;counter
+loop:
+	mov r5, r2 ;temp
+	add r2, r2, r3
+	mov r3, r5
+	cmp r4, r1
+	blt loop
+	mov r1, r2
+print:
+	ldr r0, =outMsg
 	bl printf
+	
 	pop {lr}
 	bx lr
 	/*
