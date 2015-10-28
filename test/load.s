f04c8a2a9fa09f9de680d488e98f5b9dba94b926
Michael Risher <rishermichael@gmail.com>
2015-10-28 15:14:03 -0700

    class

--- test/load.s
+++ test/load.s
@@ -1,9 +1,24 @@
-.global main
-main:
-	adr r0, value
-	mov r1, #15
-exit:
-	mov r7, #15
+.global _start
+_start:
+	ldr r1, =string
+	ldr r3, =numbers
+	mov r2, #26
+	
+_loop:
+	ldrb r0, [r1, r2]
+	strb r0, [r3, r2]
+	subs r2, r2, #1
+	bpl _loop
+_write
+	mov r0, #1
+	ldr r1, =numbers
+	mov r2, #26
+	mov r7, #4
+	swi 0
+_exit:
+	mov, #1
 	swi 0
 	
-value: .word 255
+.data
+string: .asciz "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers: .asciz "01234567891011121314151617"
