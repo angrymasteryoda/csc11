082db82b0771010d2ba27880e5884a88ab8355f3
Michael Risher <rishermichael@gmail.com>
2015-10-28 17:23:40 -0700

    class

--- test/test.s
+++ test/test.s
@@ -30,27 +30,6 @@ main:
 	bx lr
 	
 inputAddr: .asciz input
-/*main:
-	push { lr}
-	bl getchar
-	mov r1, r0
-	ldr r0, =test
-	bl printf
-cmpa:
-	ldr r2, =comp
-	ldrb r2, [r2]
-	cmp r1, r2
-	beq true
-	b end
-true:
-	ldr r0, =trueM
-	bl printf
-end:
-	pop {lr}
-	bx lr
-	*/
 
-
-.global getchar
 .global scanf
 .global printf
