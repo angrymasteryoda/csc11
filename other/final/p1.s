9854700eca7c717a831f828f0b11b855acc7a3ff
Michael Risher <rishermichael@gmail.com>
2015-12-10 11:12:08 -0800

    fixing derp mistake 2

---
+++ other/final/p1.s
@@ -0,0 +1,32 @@
+.data
+1k: .word 1000
+m: .asciz "%d\n"
+
+.text
+
+.global main /*p1*/
+
+p1: /*main:*/
+push {lr}
+
+mov r0, #0
+bl time
+bl srand
+bl rand
+mov r1, r0, asr #1
+ldr r2, =1k
+ldr r2, [r2]
+bl divMod
+@mov r0, r1
+
+ldr r0, =m
+bl printf
+
+
+
+pop {lr}
+bx lr
+.global divMod
+.global time
+.global srand
+.global rand
