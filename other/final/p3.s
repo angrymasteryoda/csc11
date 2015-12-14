f5ffc8a717b6baee7c685f1cd1a79a44b5b62065
Michael Risher <rishermichael@gmail.com>
2015-12-13 16:14:11 -0800

    stuff

--- other/final/p3.s
+++ other/final/p3.s
@@ -18,7 +18,7 @@ main:
 	@load input to r1
 	ldr r1, =in
 	ldr r1, [r1]
-	
+	/*
 	@load the scaled ints
 	ldr r2, =0x12b0  @bp-16 wd 16
 	ldr r3, =0xe042  @bp-16 wd 16
@@ -33,6 +33,6 @@ main:
 	@output result
 	ldr r0, =result
 	mov r1, r0
-	bl printf
+	bl printf*/
 inAddr: .word in
 .global printf
