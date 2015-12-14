32db76039bdc4cafc9a925056403be1bb7251341
Michael Risher <rishermichael@gmail.com>
2015-12-14 11:42:08 -0800

    final

--- other/final/p4.s
+++ other/final/p4.s
@@ -13,12 +13,12 @@ loop:
 	@load floats
 	ldr r3, =a
 	vldr s6, [r3]
-	ldr r4, =b
+/*	ldr r4, =b
 	vldr s8, [r4]
 	
 	vmov s15, r2
 	vcvt.f32.s32 s4, s15
-	/*
+
 	vmul.f32 s2, s4, s4 @ x*x
 	vmul.f32 s2, s2, s6 @ a*(x^2)
 	vmul.f32 s10, s8, s4 @ b * x
@@ -38,5 +38,7 @@ loop:
 end:
 	pop {lr}
 	bx lr
+aAddr: .word a
+bAddr: .word b
 .global printf
 
