11c21a0d460583490340a709c4312a3c23c7dd9e
Michael Risher <rishermichael@gmail.com>
2015-12-14 12:07:15 -0800

    final

--- other/final/p3.s
+++ other/final/p3.s
@@ -6,27 +6,25 @@ result: .asciz "result: %d\n"
 
 main:
 	push {lr}
-	mov r2, #0
+	mov r10, #0
 loop:
 	@load the scaled ints
 	ldr r3, =0x12b0  @bp-16 wd 16
 	ldr r4, =0xe042  @bp-16 wd 16
 		
 	@do the amth
-	mul r1, r2, r2 @ x*x
+	mul r1, r10, r10 @ x*x
 	mul r1, r1, r3 @ a*(x^2)
-	mul r5, r4, r2 @ b * x
+	mul r5, r4, r10 @ b * x
 	add r1, r1, r5 @ (a*(x^2)) + (b*x)
 	
 	@output result
-	push {r2}
 	lsr r1, #16
 	ldr r0, =result
 	bl printf
-	pop {r2}
 	@add to counter
-	add r2, r2, #1
-	cmp r2, #255
+	add r10, r10, #1
+	cmp r10, #255
 	ble loop
 end:
 	pop {lr}
