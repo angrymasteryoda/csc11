c1b5e911b5e1a74402fb056cbf32c14553ebd560
Michael Risher <rishermichael@gmail.com>
2015-12-14 11:50:19 -0800

    final

--- other/final/p4.s
+++ other/final/p4.s
@@ -11,11 +11,11 @@ main:
 	mov r2, #6
 loop:
 	@load floats
-	ldr r3, a
+	ldr r3, aAddr
 	vldr s6, [r3]
-/*	ldr r4, =b
+	ldr r4, bAddr
 	vldr s8, [r4]
-	
+
 	vmov s15, r2
 	vcvt.f32.s32 s4, s15
 
@@ -23,15 +23,14 @@ loop:
 	vmul.f32 s2, s2, s6 @ a*(x^2)
 	vmul.f32 s10, s8, s4 @ b * x
 	vadd.f32 s2, s2, s10 @ (a*(x^2)) + (b*x)
-	*/
-	push {r2}
+
+	/*push {r2}*/
 	ldr r0, =result
-	@vcvt.f64.f32 d0, s2
-	vcvt.f64.f32 d0, s4
+	/*@vcvt.f64.f32 d0, s2*/
+	vcvt.f64.f32 d0, s2
 	vmov r1, r2, d0
 	bl printf
-	pop {r2}
-	/*
+	/*pop {r2}
 	add r2, r2, #1
 	cmp r2, #255
 	ble loop*/
