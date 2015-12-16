69daf5b4c5bce330852a4846dd7f6028d469e72a
Michael Risher <rishermichael@gmail.com>
2015-12-16 13:09:29 -0800

    all changed

--- other/final/all.s
+++ other/final/all.s
@@ -1,49 +1,54 @@
 .data
 
-inputMsg: .asciz "Enter menu number or 5 to exit: "
-scanPatt: .asciz "%d"
-.balign 4
-input: .word 0
-test: .asciz "entered %d\n"
+@inputMsg: .asciz "Enter menu number or 5 to exit: "
+@scanPatt: .asciz "%d"
+@.balign 4
+@input: .word 0
+@test: .asciz "entered %d\n"
 
 a: .float 0.073
 b: .float 0.876
 result: .asciz "result: %f\n"
+m: .asciz "%d\n"
+mf: .asciz "%f\n"
 .text
 p4:
-	push {r4, lr}
+	push {r4-r12, lr}
 	mov r10, #0
 loop:
 	@load floats
 	ldr r3, aAddr
-	vldr s6, [r3]
+	vldr s6, [r3]  @ load a into float register
 	ldr r4, bAddr
-	vldr s8, [r4]
+	vldr s8, [r4]  @ load b into float register
 
-	vmov s15, r10
-	vcvt.f32.s32 s4, s15
-
+	vmov s15, r10 @ mov x to a float register
+	vcvt.f32.s32 s4, s15 @convert to float type
+/*
 	vmul.f32 s2, s4, s4 @ x*x
 	vmul.f32 s2, s2, s6 @ a*(x^2)
 	vmul.f32 s10, s8, s4 @ b * x
 	vadd.f32 s2, s2, s10 @ (a*(x^2)) + (b*x)
-
+*/
+	@print out the result
 	ldr r0, =result
-	vcvt.f64.f32 d0, s2
-	vmov r1, r2, d0
+	vcvt.f64.f32 d0, s4 @ convert the result in s2 to a double for printing
+	vmov r1, r2, d0 @ mov to int registers to print
 	bl printf
 
-	add r10, r10, #1
-	cmp r10, #255
+	add r10, r10, #1 @ x++
+	cmp r10, #255 @ x < 255
 	ble loop
 
 end:
-	pop {r4, lr}
+	pop {r4-r12, lr}
 	bx lr
 	
 .global main
 main:
-	push {r4, lr}
+	push {r4-r12, lr}
+bl p4
+/*
 menuloop:
 	ldr r0, =inputMsg
 	bl printf
@@ -60,12 +65,12 @@ menuloop:
 	bal exit
 problem4:
 	bl p4
-	b menuloop
-exir:
-	pop {r4, lr}
+	b menuloop*/
+exit:
+	pop {r4-12, lr}
 	bx lr
 	
-inputAddr: .word input
+@inputAddr: .word input
 aAddr: .word a
 bAddr: .word b
 .global printf
