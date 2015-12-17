8cecc0d1858dfeb1b31e9e3fa1bf1a03f9c83b48
Michael Risher <rishermichael@gmail.com>
2015-12-16 17:47:06 -0800

    stuff

--- other/final/p3.s
+++ other/final/p3.s
@@ -5,7 +5,7 @@ result: .asciz "result: %d\n"
 .global p3
 
 p3:
-	push {r4, lr}
+	push {r4-r12, lr}
 	mov r10, #0
 loop:
 	@load the scaled ints
@@ -27,6 +27,6 @@ loop:
 	cmp r10, #255
 	ble loop
 end:
-	pop {r4, lr}
+	pop {r4-r12, lr}
 	bx lr
 .global printf
